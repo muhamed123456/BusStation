c5f309206f44ae71d2449d2ccd937dcf635a33f4
Muhammed Amzovski <muhammedamzovski96@gmail.com>
2017-12-20 19:32:43 +0100

    Using NuGet Package Manager - Package Manager Console, created BusStationContext

---
+++ BusStation/BusStation/Migrations/20171220182844_Initial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BusStation.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ArrivalTime = table.Column<DateTime>(nullable: false),
+                    BusCompany = table.Column<string>(nullable: true),
+                    DepartureCity = table.Column<string>(maxLength: 60, nullable: false),
+                    DepartureTime = table.Column<DateTime>(nullable: false),
+                    DestinationCity = table.Column<string>(maxLength: 60, nullable: false),
+                    Price = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cities");
+        }
+    }
+}
