3134a675bf70b4ef4fd46bae7bb7980ec8c3970e
Muhammed Amzovski <muhammedamzovski96@gmail.com>
2017-12-26 13:48:42 +0100

    Created new API controller with Entity Framework. Added a search glyphicon.

--- BusStation/BusStation/Controllers/CitiesAPIController.cs
+++ BusStation/BusStation/Controllers/CitiesAPIController.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using BusStation.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BusStation.Models;
 
 namespace BusStation.Controllers
 {
@@ -12,122 +13,113 @@ namespace BusStation.Controllers
     [Route("api/CitiesAPI")]
     public class CitiesAPIController : Controller
     {
-        // GET: api/GetAllCities  
-        [HttpGet]
-        public IEnumerable<Cities> GetAllCities()
+        private readonly BusStationContext _context;
+
+        public CitiesAPIController(BusStationContext context)
         {
-            List<Cities> cities = new List<Cities>
-           {
-                new Cities
-                     {
-                         DepartureCity = "Resen",
-                         DestinationCity = "Ohrid",
-                         DepartureTime = DateTime.Parse ("01.01.2018 08:00:00"),
-                         ArrivalTime = DateTime.Parse ("01.01.2018 08:45:00"),
-                         BusCompany = "Galeb",
-                         Price = 150
-                     },
-                     new Cities
-                     {
-                         DepartureCity = "Resen",
-                         DestinationCity = "Ohrid",
-                         DepartureTime = DateTime.Parse("01.01.2018 09:30:00"),
-                         ArrivalTime = DateTime.Parse("01.01.2018 10:15:00"),
-                         BusCompany = "Transkop",
-                         Price = 140
-                     },
-                     new Cities
-                     {
-                         DepartureCity = "Ohrid",
-                         DestinationCity = "Resen",
-                         DepartureTime = DateTime.Parse("01.01.2018 13:00:00"),
-                         ArrivalTime = DateTime.Parse("01.01.2018 13:45:00"),
-                         BusCompany = "Galeb",
-                         Price = 150
-                     },
-                     new Cities
-                     {
-                         DepartureCity = "Ohrid",
-                         DestinationCity = "Resen",
-                         DepartureTime = DateTime.Parse("01.01.2018 15:00:00"),
-                         ArrivalTime = DateTime.Parse("01.01.2018 15:45:00"),
-                         BusCompany = "Transkop",
-                         Price = 140
-                     },
-                     new Cities
-                     {
-                         DepartureCity = "Resen",
-                         DestinationCity = "Bitola",
-                         DepartureTime = DateTime.Parse("01.01.2018 08:00:00"),
-                         ArrivalTime = DateTime.Parse("01.01.2018 08:30:00"),
-                         BusCompany = "Transkop",
-                         Price = 90
-                     },
-                     new Cities
-                     {
-                         DepartureCity = "Resen",
-                         DestinationCity = "Bitola",
-                         DepartureTime = DateTime.Parse("01.01.2018 10:00:00"),
-                         ArrivalTime = DateTime.Parse("01.01.2018 10:30:00"),
-                         BusCompany = "Galeb",
-                         Price = 100
-                     },
-                     new Cities
-                     {
-                         DepartureCity = "Bitola",
-                         DestinationCity = "Resen",
-                         DepartureTime = DateTime.Parse("01.01.2018 16:00:00"),
-                         ArrivalTime = DateTime.Parse("01.01.2018 16:30:00"),
-                         BusCompany = "Transkop",
-                         Price = 90
-                     },
-                     new Cities
-                     {
-                         DepartureCity = "Bitola",
-                         DestinationCity = "Resen",
-                         DepartureTime = DateTime.Parse("01.01.2018 18:00:00"),
-                         ArrivalTime = DateTime.Parse("01.01.2018 18:45:00"),
-                         BusCompany = "Galeb",
-                         Price = 100
-                     },
-                     new Cities
-                     {
-                         DepartureCity = "Resen",
-                         DestinationCity = "Skopje",
-                         DepartureTime = DateTime.Parse("01.01.2018 06:45:00"),
-                         ArrivalTime = DateTime.Parse("01.01.2018 10:15:00"),
-                         BusCompany = "Galeb",
-                         Price = 550
-                     },
-                     new Cities
-                     {
-                         DepartureCity = "Resen",
-                         DestinationCity = "Skopje",
-                         DepartureTime = DateTime.Parse("01.01.2018 08:00:00"),
-                         ArrivalTime = DateTime.Parse("01.01.2018 12:00:00"),
-                         BusCompany = "Transkop",
-                         Price = 520
-                     },
-                     new Cities
-                     {
-                         DepartureCity = "Skopje",
-                         DestinationCity = "Resen",
-                         DepartureTime = DateTime.Parse("01.01.2018 16:00:00"),
-                         ArrivalTime = DateTime.Parse("01.01.2018 19:30:00"),
-                         BusCompany = "Galeb",
-                         Price = 550
-                     },
-                     new Cities
-                     {
-                         DepartureCity = "Skopje",
-                         DestinationCity = "Resen",
-                         DepartureTime = DateTime.Parse("01.01.2018 17:30:00"),
-                         ArrivalTime = DateTime.Parse("01.01.2018 21:30:00"),
-                         BusCompany = "Transkop",
-                         Price = 520
-                     }
-           };
-            return cities;
+            _context = context;
+        }
+
+        // GET: api/CitiesAPI
+        [HttpGet]
+        public IEnumerable<Cities> GetCities()
+        {
+            return _context.Cities;
+        }
+
+        // GET: api/CitiesAPI/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCities([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var cities = await _context.Cities.SingleOrDefaultAsync(m => m.ID == id);
+
+            if (cities == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cities);
+        }
+
+        // PUT: api/CitiesAPI/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCities([FromRoute] int id, [FromBody] Cities cities)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != cities.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cities).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CitiesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/CitiesAPI
+        [HttpPost]
+        public async Task<IActionResult> PostCities([FromBody] Cities cities)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Cities.Add(cities);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCities", new { id = cities.ID }, cities);
+        }
+
+        // DELETE: api/CitiesAPI/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCities([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var cities = await _context.Cities.SingleOrDefaultAsync(m => m.ID == id);
+            if (cities == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cities.Remove(cities);
+            await _context.SaveChangesAsync();
+
+            return Ok(cities);
+        }
+
+        private bool CitiesExists(int id)
+        {
+            return _context.Cities.Any(e => e.ID == id);
         }
     }
 }
